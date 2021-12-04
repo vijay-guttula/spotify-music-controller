0cb4342328bd920c13ea964f6ecb15f1d1acdf75
Vijay Guttula <ssvijayg@gmail.com>
2021-12-04 19:26:15 +0530

    models edit

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,9 @@ class RoomSerializer(serializers.ModelSerializer):
   class Meta:
     model = Room
     fields = ('id','code','host','guest_can_pause', 'votes_to_skip','created_at') 
+    
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Room
+    fields = ('guests_can_pause', 'votes_to_skip')
--- api/views.py
+++ api/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
   queryset = Room.objects.all()
   serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+  serializer_class = CreateRoomSerializer
+  
+  def post(self, request, format=None):
+    pass
