672fc8a4b9103f11ed16c5924f7c42f191f2c109
Vijay Guttula <ssvijayg@gmail.com>
2021-12-04 20:54:23 +0530

    fixed code return bug

--- api/models.py
+++ api/models.py
@@ -10,7 +10,7 @@ def generate_unique_code():
     if Room.objects.filter(code=code).count() == 0:
       break
     
-    return code
+  return code
 
 # Create your models here.
 class Room(models.Model):
